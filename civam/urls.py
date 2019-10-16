2a887b82023af307a6e1677053160844f6a8485a
Juan Villacis <juanfv@Juans-MBP-2.fios-router.home>
2019-10-15 23:03:26 -0400

    Added urls for items

--- civam/urls.py
+++ civam/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'),#/collection/
+    path('<int:item_id>/', view.detail, name='detail') #/collection/<item>/
+    path('new/', views.new, name='new' ) #/collection/new 
 ]
