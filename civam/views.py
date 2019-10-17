2363738084d4274bcf57544e63caa70667e0300d
Juan Villacis <juanfv@Juans-MBP-2.fios-router.home>
2019-10-16 20:42:54 -0400

    added view for individual item, with its corresponding stories showing

--- civam/views.py
+++ civam/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Item
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import HttpResponse
+from .models import *
 
 # Create your views here.
 def index(request):
@@ -10,5 +11,8 @@ def index(request):
 def new(request):
     return HttpResponse("New item")
 
-def detail(request):
-    return HttpResponse("detail")
+def detail(request, item_id):
+    item = get_object_or_404(Item, pk=item_id)
+    stories = Story.objects.filter(item_id=item_id)
+    context = {'item' : item, 'stories': stories}
+    return render(request, 'civam/item.html', context)
