620cce4023cb757eba4bffca2f59c5152cacc95d
Matthew Zelinsky <zelinsky@udel.edu>
2019-12-01 15:12:59 -0500

    Added comments to admin and models.

--- django_project/civam/admin.py
+++ django_project/civam/admin.py
@@ -1,18 +1,25 @@
 from django.contrib import admin
-from .models import Collection, Item, Image, Video, Story, CollectionGroup
+from .models import Collection, Item, Image, Video, Story
 from guardian.admin import GuardedModelAdmin
 
-# Register your models here
+# Civam admin models are defined here
+# The admin module has capabilities to created/edit/view/delete Collections, Items, Images, Videos, and Stories
+
+
+# A default admin model used for models with created_by, created_on, modified_by, and modified_on fields
 class DefaultAdmin(GuardedModelAdmin):
     readonly_fields = ('created_by', 'created_on', 'modified_by', 'modified_on')
 
-    
+
     '''
+    # User have permission to change objects that they own
     def has_change_permission(self, request, obj=None):
         if obj is not None and obj.created_by and not request.user.is_superuser and obj.created_by != request.user:
             return False
         return True
     '''
+
+    # Sets created_by and modified_by fields
     def save_model(self, request, instance, form, change):
         user = request.user 
         instance = form.save(commit=False)
@@ -23,11 +30,13 @@ class DefaultAdmin(GuardedModelAdmin):
         form.save_m2m()
         return instance
 
+# Items can be created within a Collection
 class ItemInline(admin.TabularInline):
     model = Item
     exclude = ['created_by', 'created_on', 'modified_by', 'modified_on',]
     #can_delete = False
 
+# Images, Videos, and Stories can only be created within an Item
 class ImageInline(admin.TabularInline):
     model = Image
 
@@ -38,29 +47,17 @@ class StoryInline(admin.TabularInline):
     model = Story
     exclude = ['created_by', 'created_on', 'modified_by', 'modified_on',]
 
-class CollectionGroupInline(admin.TabularInline):
-    model = CollectionGroup
 
+# Can create Collections and Items directly
 class CollectionAdmin(DefaultAdmin):
     list_display = ('title', 'created_by')
-    inlines = [CollectionGroupInline,]
 
 class ItemAdmin(DefaultAdmin):
     list_display = ('name', 'collection')
     inlines = [ImageInline, VideoInline, StoryInline,]
 
-class StoryAdmin(DefaultAdmin):
-    list_display = ('item', 'created_by')
-
-class CollectionGroupAdmin(admin.ModelAdmin):
-    pass
-
-
-    
-    
+# Register admin models    
 admin.site.register(Collection, CollectionAdmin)
 admin.site.register(Item, ItemAdmin)
-admin.site.register(Story, StoryAdmin)
-admin.site.register(CollectionGroup, CollectionGroupAdmin)
 
     
