6f29a834e8dd505ca3f7489b570ed9ae9d6bd442
Matthew Zelinsky <zelinsky@udel.edu>
2019-12-02 16:00:35 -0500

    Added more comments.

--- django_project/civam/forms.py
+++ django_project/civam/forms.py
@@ -4,6 +4,8 @@ from civam.models import *
 from guardian.models import Group
 from django.core.exceptions import NON_FIELD_ERRORS
 
+# Civam forms are defined here
+
 class StoryForm(ModelForm):
     class Meta:
         model = Story
@@ -20,28 +22,35 @@ class ItemForm(ModelForm):
         model = Item
         fields = ['name', 'description']
 
+# Displayed on same page as ItemForm
 class ImageForm(forms.Form):
     content = forms.ImageField(required=False)
-   
+
+# Displayed on same page as ItemForm
 class VideoForm(forms.Form):
     link = forms.URLField(required=False)
 
+    
 class CollectionGroupForm(ModelForm):
     class Meta:
         model = CollectionGroup
         fields = ['name', 'default', 'collection']
-        widgets = {'collection': forms.HiddenInput()}
+        widgets = {'collection': forms.HiddenInput()} # The collection is a hidden input that is automatically filled
         error_messages = {
             NON_FIELD_ERRORS: {
                 'unique_together': "A group with that name already exists for this collection.",
             }
         }
 
-
+# The form for assigning permissions to a CollectionGroup
+# Seperate form object but displayed on the same page as CollectionGroupForm
 class GroupPermissionsForm(forms.Form):
+
+    # List objects belonging the collection specified by collection_id
     def __init__(self,*args,**kwargs):
         collection_id = kwargs.pop('collection_id')
         super(GroupPermissionsForm,self).__init__(*args,**kwargs)
         self.fields['items'].queryset = Item.objects.filter(collection_id=collection_id)
-    
+
+    # Items in the collection can be checked/unchecked to grant/remove permission on that Item for the CollectionGroup
     items = forms.ModelMultipleChoiceField(queryset = Item.objects.none(), widget = forms.CheckboxSelectMultiple, required=False)
--- django_project/civam/urls.py
+++ django_project/civam/urls.py
@@ -3,13 +3,28 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # Display collections
+    path('', views.collection_list, name='index'),
     path('collections/', views.collection_list, name='collections'),#/collection/
+
+    # Make a new collection
     path('collections/new/', views.new_collection, name='new_collection'),
+
+    # View a collection by collection_id
     path('collections/<int:collection_id>/', views.collection, name='collection'),
+
+    # Create a new item within collection
     path('collections/<int:collection_id>/new/', views.new_item, name='new_item'),
+
+    # View an item within a collection
     path('collections/<int:collection_id>/<int:item_id>/', views.item, name='item'),
+
+    # View CollectionGroups of a Collection
     path('collections/<int:collection_id>/groups/', views.group_list, name='groups'),
+
+    # Add a new CollectionGroup to a Collection
     path('collections/<int:collection_id>/groups/new/', views.new_group, name='new_group'),
+
+    # View/Edit a CollectionGroup
     path('collections/<int:collection_id>/groups/<int:group_id>/', views.group, name='group')
 ]
