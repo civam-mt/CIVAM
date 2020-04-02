29560fce077cc5e2f240b826560b84cae2b20b59
Joy Kitson <tkitson@udel.edu>
2020-04-02 15:09:16 -0400

    added about module

--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 
 const routes: Routes = [
 {path:'',redirectTo:'home',pathMatch:'full'},
-{path:'home',component:HomeComponent}
+{path:'home',component:HomeComponent},
+{path:'about',component:AboutComponent}
 ];
 
 @NgModule({
