12325a4716d331d0407707b9c36c089ad94300bb
Joy Kitson <tkitson@udel.edu>
2020-04-10 21:09:49 -0400

    now adding first three District mock collections to the home page automatically

---
+++ angular-frontend/src/app/mock-collections.ts
@@ -0,0 +1,29 @@
+import { Collection } from './collection'
+
+export const DISTRICTS: Collection[] = [
+    { 
+        name: 'Big Horn',
+        description: 'Pictures from the Big Horn District', 
+        image: 'https://tse1.mm.bing.net/th?id=OIP.nWZRFJ3J0yU83QK1BFnmtwHaFj&pid=Api' 
+    },{ 
+        name: 'Black Lodge',
+        description: 'Pictures from the Black Lodge District', 
+        image: 'https://tse4.mm.bing.net/th?id=OIP.2gKxCyFVDLWattvdVcoUMgHaE8&pid=Api' 
+    },{ 
+        name: 'Lodge Grass',
+        description: 'Pictures from the Lodge Grass District', 
+        image: 'https://bloximages.chicago2.vip.townnews.com/billingsgazette.com/content/tncms/assets/v3/editorial/5/13/513a9c2f-3fba-50bd-968f-407a4bdf1c48/4ecb4c0a7dbe6.image.jpg' 
+    },{ 
+        name: 'Pryor',
+        description: 'Pictures from the Pryor District', 
+        image: 'http://www.trbimg.com/img-5849a921/turbine/la-me-richard-pryor-20051211' 
+    },{ 
+        name: 'Reno',
+        description: 'Pictures from the Reno District', 
+        image: 'https://housely.com/wp-content/uploads/2016/07/Reno-NV.png' 
+    },{ 
+        name: 'Wyola',
+        description: 'Pictures from the Wyola District', 
+        image: 'http://media6.trover.com/T/55bd9e248e7cb234800036d4/fixedw_large_4x.jpg' 
+    }
+]
