3ec951c6942ee77d539670feff3ec84f13ea665b
Sophia Freaney <sfreaney@udel.edu>
2020-04-30 15:17:25 -0400

    better json object access - from Joy

--- angular-frontend/src/app/home/home.component.ts
+++ angular-frontend/src/app/home/home.component.ts
@@ -14,7 +14,6 @@ export class HomeComponent implements OnInit {
   featuredCollections: Collection[] = DISTRICTS.slice(0, 3);
   /* TODO : Uncomment API Stuff*/
   API_URL = environment.apiUrl;
-  tempCollections;
   collections;
 
   constructor(private api: ApiService) { }
@@ -24,9 +23,8 @@ export class HomeComponent implements OnInit {
   }
   getCollections() {
       this.api.getCollections().subscribe((data) => {
-      this.tempCollections = data;
-      this.collections = this.tempCollections.collection_list;
-      console.log(this.collections);
+        console.log(data);
+        this.collections = data["collection_list"];
     });
   }
 
