5866c1378b1782352784fdfdb56b41e3a65d5217
Jimmy <jimmyskripchuk@gmail.com>
2020-05-24 12:15:03 -0400

    added all items view 'api/items/all'

--- django_project/civam/urls.py
+++ django_project/civam/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('api/collections/<int:collection_id>/<int:item_id>/', api_views.item, name='api_item'),
 
     path('api/items/<int:item_id>/', api_views.item_solo, name='api_item_solo'),
+    path('api/items/all/', api_views.all_items, name='api_all_items'),
     # View an item within a collection
     path('collections/<int:collection_id>/<int:item_id>/', views.item, name='item'),
     path('api/collections/<int:collection_id>/<int:item_id>/', api_views.item, name='api_item'),
