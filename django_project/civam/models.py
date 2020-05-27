3ac2e40984b6d3170cea98efa3167f67774664ce
Jimmy <jimmyskripchuk@gmail.com>
2020-05-26 20:21:00 -0400

    minor admin fixes

--- django_project/civam/models.py
+++ django_project/civam/models.py
@@ -94,7 +94,7 @@ class Item(models.Model):
     
 
     cataloger = models.CharField(max_length=511, null=True, blank=True)
-    catalog_date = models.CharField(max_length=511, null=True, blank=True)    
+    catalog_date = models.DateTimeField(blank=True, null=True)
     created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="items_created")
     created_on = models.DateTimeField(auto_now_add=True)
     modified_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="items_modified")
