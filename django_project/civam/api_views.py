9599d2ea39184f982e179c8685cbe0883188a469
evdogg123 <evanmac@udel.edu>
2020-10-13 16:19:56 -0400

    cleaned up stuff

--- django_project/civam/api_views.py
+++ django_project/civam/api_views.py
@@ -43,11 +43,7 @@ def register(request):
 def searchResult(request):
 	print("In searchResults")
 	query = request.GET.get('data', None)
-	logger.warn(request.GET.get('data', None))
-	items = Item.objects.all()
-	#item_list = list(item_list.values())
 	item_list = []
-	#keywords = Keyword.objects.annotate(similarity=TrigramSimilarity('word', str(query)) ).filter(similarity__gt=0.3).order_by('-similarity')
 	matched_keywords = Keyword.objects.filter(word__trigram_similar=query)
 	print(list(matched_keywords))
 	items =Item.objects.filter(keywords__in= list(matched_keywords))
