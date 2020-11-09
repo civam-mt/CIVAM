afcccf4264f60241ada942b582b784894e4f1a18
jbelak <jbelak@udel.edu>
2020-11-09 15:37:50 -0500

    adding map features

--- angular-frontend/src/app/map/full-map/full-map.component.ts
+++ angular-frontend/src/app/map/full-map/full-map.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./full-map.component.scss']
 })
 export class FullMapComponent implements OnInit {
-
+  width;
   constructor() { }
 
   ngOnInit(): void {
+    this.width = screen.width;
   }
 
 }
