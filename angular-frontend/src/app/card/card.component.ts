4b15475b9381e564edf2c421fa9e28461ba45613
Sam Zabaldo <sam.zabaldo@gmail.com>
2020-11-09 14:45:39 -0500

    separated collection cards to be their own component

--- angular-frontend/src/app/card/card.component.ts
+++ angular-frontend/src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Collection } from '../collection'
 import { DISTRICTS } from '../mock-collections';
 import { environment } from '../../environments/environment';
@@ -13,18 +13,13 @@ export class CardComponent implements OnInit {
   tempCollections: Collection[] = DISTRICTS;
 
   API_URL = environment.apiUrl;
-  collections;
+  @Input() cover_image; 
+  @Input() title; 
+  @Input() id; 
 
   constructor(private api: ApiService) { }
 
   ngOnInit(): void {
-    this.getCollections();
   }  
-  getCollections() {
-    this.api.getCollections().subscribe((data) => {
-      console.log(data);
-      this.collections = data["collection_list"];
-  });
-}
 
 }
