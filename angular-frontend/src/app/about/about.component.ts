29c9dcd12477f3ea1ed96741a1cce79d5b70fd41
Joshua Davis <jhdavis@udel.edu>
2021-04-01 12:47:58 -0400

    Add ABOUT and MISSION text retrieval from back end in front end

--- angular-frontend/src/app/about/about.component.ts
+++ angular-frontend/src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-about',
@@ -7,17 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private api: ApiService) { }
 
   public missionCollapsed = true;
   public originCollapsed = true;
   public peopleCollapsed = true;
   public historyCollapsed = true;
   public resourcesCollapsed = true;
+
+  public siteTextIDs = ['ABOUT', 'MISSION'];
+  public siteTexts = [];
   
   ngOnInit(): void {
+    this.getSiteTexts();
   }
-
+  getSiteTexts() {
+    for (var i = 0; i < this.siteTextIDs.length; i++) {
+      this.api.getSiteTextByLocation(this.siteTextIDs[i]).subscribe((data) => {
+	this.siteTexts.push(data["content"]);
+      });
+    }
+  }
 }
 
 
