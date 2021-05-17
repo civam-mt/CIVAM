6d8b3476a68cff6fa5f70b157dd999a776abcd35
Timothy Mazzarelli <tmazzar@udel.edu>
2021-05-17 17:56:37 -0400

    search result done

--- angular-frontend/src/app/search-result/search-result.component.ts
+++ angular-frontend/src/app/search-result/search-result.component.ts
@@ -16,17 +16,33 @@ export class SearchResultComponent implements OnInit {
   items;
   query;
   router_route = '/items';
+  currentSubPage:string;
+  currentPageUrl:string = '';
+  allowedSubPage:string[] = ['gall', 'list'];
+  pageSize;
+  pageOfItems;
   constructor(private route: ActivatedRoute, private api: ApiService) {
 
   }
 
   ngOnInit(): void {
+    this.pageSize = 9;
     this.route.params.subscribe(params => {
       this.query = params['data']
     });
     this.getItems(this.query);
     this.route.params.subscribe(params => {
       this.handleChange(params['data']);
+    });
+    this.route.url.subscribe((url) => {
+      this.currentPageUrl = '';
+      url.forEach((urlComp) => {this.currentPageUrl = this.currentPageUrl + '/' + urlComp.path + ";data=" + this.query});
+    });
+    this.route.fragment.subscribe((fragment: string) => {
+      if (this.allowedSubPage.includes(fragment)) this.currentSubPage = fragment;
+      else this.currentSubPage = this.allowedSubPage[0];
+    }, (nonString:any) => {
+      this.currentSubPage = this.allowedSubPage[0];
     });
   }
 
@@ -39,6 +55,14 @@ export class SearchResultComponent implements OnInit {
     this.api.getSearch(query).subscribe((data) => {
       this.items = data["items"];
     });
+  }
+
+  onChangePage(pageOfItems) {
+    // update current page of items
+    this.pageOfItems = pageOfItems;
+  }
+  changePageSize(event){
+    this.pageSize = event.value;
   }
 }
 
