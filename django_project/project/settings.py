8a6aeda93df88c7a0b7c85def07506d2100d1d11
Nitin Armstrong <nitin.armstrong@gmail.com>
2023-03-13 10:23:15 -0400

    changed settings.py

--- django_project/project/settings.py
+++ django_project/project/settings.py
@@ -46,8 +46,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # UNCOMMENT FOLLOWING 2 LINES FOR LOCAL DEVELOPMENT ONLY!!!!
-#ALLOWED_HOSTS = ['198.211.99.20', 'localhost:8000', '127.0.0.1', 'civam-mt.org','localhost:4200', "*"]
-#STATIC_ROOT = os.path.join('~/CIVAM/django_project', 'static/')
+ALLOWED_HOSTS = ['198.211.99.20', 'localhost:8000', '127.0.0.1', 'civam-mt.org','localhost:4200', "*"]
+STATIC_ROOT = os.path.join('~/CIVAM/django_project', 'static/')
 
 # Local Development SMPT Server
 # Command to launch: python -m smtpd -n -c DebuggingServer localhost:1025
